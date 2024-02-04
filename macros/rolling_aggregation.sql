7dd746e4290f1ef8a2c24a409f0f4565e37b7518
Eloise Jones <eloise.jones@vouch.us>
2024-02-04 20:06:32 +0000

    Use rolling_aggregation in a model

--- macros/rolling_aggregation.sql
+++ macros/rolling_aggregation.sql
@@ -3,11 +3,15 @@
     partition_by,
     order_by='created_at',
     aggregation_function='avg',
-    num_rows_preceding=6
+    num_rows_preceding=6,
+    output_column_name=''
 ) %}
+    {% set output_column_name_template = 'rolling_' ~ aggregation_function~ '_' ~ (num_rows_preceding+1)|string ~ '_rows_' ~ column_name %}
+    {% set output_column_name_final = output_column_name if output_column_name != '' else output_column_name_template %}
+
     {{ aggregation_function }}( {{ column_name }} ) OVER (
                 PARTITION BY {{ partition_by }}
                 ORDER BY {{ order_by }}
-                ROWS BETWEEN num_rows_preceding PRECEDING AND CURRENT ROW
-            ) AS rolling_{{aggregation_function}}_{{num_rows_preceding+1}}_rows_{{ column_name }}
+                ROWS BETWEEN {{ num_rows_preceding|string }} PRECEDING AND CURRENT ROW
+            ) AS {{ output_column_name_final }}
 {% endmacro %}
